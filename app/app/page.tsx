f70e7640aa9c003850c69866eddb2c941d99aaf5
niars091-Vdn <niars091@gmail.com>
2026-06-04 23:37:48 +0200

    Update page.tsx

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,10 +1,1 @@
-export default function Home() {
-  return (
-    <html>
-      <head>
-        <meta httpEquiv="refresh" content="0; url=/login" />
-      </head>
-      <body></body>
-    </html>
-  )
-}
+export { default } from '../page'
