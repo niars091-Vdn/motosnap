5099a7b9eecaf94ea8fdac484fe16c3f10d90a65
niars091-Vdn <niars091@gmail.com>
2026-06-06 13:17:51 +0200

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from '@supabase/ssr'
 import { createClient as createSupabaseAdmin } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://gcggrxyjovvulfgudsiq.supabase.co'
-const SUPABASE_ANON = 'INCOLLA_QUI_LA_ANON_KEY'
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjZ2dyeHlqb3Z2dWxmZ3Vkc2lxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0ODYyMzgsImV4cCI6MjA5MzA2MjIzOH0.03Y9KQ-Tf4Zee9uwPx8yTYTiHQHn9LPISSlgahqTeR0'
 
 export function createClient() {
   return createBrowserClient(SUPABASE_URL, SUPABASE_ANON)
